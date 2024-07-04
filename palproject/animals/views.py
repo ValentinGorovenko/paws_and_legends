1f1790711b6c146e2e10d236a7fc5e76f30ea359
Валентин Горовенко <Punker8381@yandex.ru>
2024-07-04 15:01:05 +0700

    добавление redirect

--- palproject/animals/views.py
+++ palproject/animals/views.py
@@ -1,5 +1,5 @@
-from django.http import Http404, HttpResponse, HttpResponseNotFound
-
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponsePermanentRedirect
+from django.urls import reverse
 
 def index(request):
     return HttpResponse('Главна страница')
@@ -15,7 +15,9 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        raise Http404()
+        uri = reverse('cats', args=('sport',))
+        return HttpResponsePermanentRedirect(uri)
+
     return HttpResponse(f"<h1>Архив по годам</h1><p >{year}</p>")
 
 
